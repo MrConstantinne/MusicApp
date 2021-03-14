17639e76b3bec138c9cc83eec1de9bd3e5f1c95d
Konstantin Maksimovich <kmaksimovich@yandex.ru>
2021-03-14 19:41:25 +0300

    add to comment & file upload

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,20 @@
-import {Module} from "@nestjs/common";
-import {TrackModule} from "./track/track.module";
-import {MongooseModule} from "@nestjs/mongoose";
+import * as path from "path";
+import { Module } from "@nestjs/common";
+import { FileModule } from "./file/file.module";
+import { MongooseModule } from "@nestjs/mongoose";
+import { TrackModule } from "./track/track.module";
+import { ServeStaticModule } from "@nestjs/serve-static";
 
 @Module({
     imports: [
+        ServeStaticModule.forRoot({
+           rootPath: path.resolve(__dirname, 'static'),
+        }),
         MongooseModule.forRoot(
             'mongodb+srv://music:pBiMfHinEiVA5v3d@1.xbwcp.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
         ),
-        TrackModule
+        TrackModule,
+        FileModule,
     ]
 })
 export class AppModule {}
--- src/track/track.service.ts
+++ src/track/track.service.ts
@@ -4,17 +4,26 @@ import { Track, TrackDocument } from "./schemas/track.schema";
 import { Model, ObjectId, Query } from "mongoose";
 import { Comment, CommentDocument } from "./schemas/comment.schema";
 import { CreateTrackDto } from "./dto/create-track.dto";
-
+import { CreateCommentDto } from "./dto/create-comment.dto";
+import { FileService, FileType } from "../file/file.service";
 
 @Injectable()
 export class TrackService {
     constructor(
         @InjectModel(Track.name) private trackModel: Model<TrackDocument>,
         @InjectModel(Comment.name) private commentModel: Model<CommentDocument>,
+        private fileService: FileService,
     ) {}
 
-    async create(dto: CreateTrackDto): Promise<Track> {
-        return this.trackModel.create({...dto, listens: 0});
+    async create(dto: CreateTrackDto, picture, audio): Promise<Track> {
+        const audioPath = this.fileService.createFile(FileType.AUDIO, audio);
+        const picturePath = this.fileService.createFile(FileType.IMAGE, picture);
+        return this.trackModel.create({
+            ...dto,
+            listens: 0,
+            audio: audioPath,
+            picture: picturePath,
+        });
     }
 
     async getAll(): Promise<Track[]> {
@@ -22,10 +31,18 @@ export class TrackService {
     }
 
     async getOne(id: ObjectId): Promise<Track> {
-        return this.trackModel.findById(id);
+        return this.trackModel.findById(id).populate('comments');
     }
 
     async delete(id: ObjectId): Promise<Query<TrackDocument | null, TrackDocument>> {
         return this.trackModel.findByIdAndDelete(id);
+    }
+
+    async addComment(dto: CreateCommentDto): Promise<Comment> {
+        const track = await this.trackModel.findById(dto.trackId);
+        const comment = await this.commentModel.create({ ...dto });
+        track.comments.push(comment._id);
+        await track.save();
+        return comment;
     }
 }
